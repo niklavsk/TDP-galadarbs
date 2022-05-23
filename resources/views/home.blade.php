d1666692cf58e72432ee20f5d343b5747ab9af79
niklavsk <niklavsk36@gmail.com>
2022-05-23 12:18:51 +0300

    Add accordion menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,149 @@
 @extends('master')
+
+@section('color')
+    dark-blue
+@endsection
+
+@section('content')
+    <div class="accordion-menu">
+        <div class="menu-item item-durvis">
+            <div class="item-contents">
+                <div class="col-left">
+                    <h3>Durvis</h3>
+                </div>
+
+                <div class="col-right">
+                    <div class="text-container">
+                        <h6>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                        </h6>
+                    </div>
+
+                    <div class="button-container">
+                        <a href="{{ url('/durvis') }}" class="button-white">Lasīt vairāk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-item item-krustojums">
+            <div class="item-contents">
+                <div class="col-left">
+                    <h3>Krustojums</h3>
+                </div>
+
+                <div class="col-right">
+                    <div class="text-container">
+                        <h6>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                        </h6>
+                    </div>
+
+                    <div class="button-container">
+                        <a href="{{ url('/krustojums') }}" class="button-white">Lasīt vairāk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-item item-galastacija">
+            <div class="item-contents">
+                <div class="col-left">
+                    <h3>Gala stacija</h3>
+                </div>
+
+                <div class="col-right">
+                    <div class="text-container">
+                        <h6>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                        </h6>
+                    </div>
+
+                    <div class="button-container">
+                        <a href="{{ url('/galastacija') }}" class="button-white">Lasīt vairāk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-item item-skatlogs">
+            <div class="item-contents">
+                <div class="col-left">
+                    <h3>Skatlogs</h3>
+                </div>
+
+                <div class="col-right">
+                    <div class="text-container">
+                        <h6>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                        </h6>
+                    </div>
+
+                    <div class="button-container">
+                        <a href="{{ url('/skatlogs') }}" class="button-white">Lasīt vairāk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-item item-brauciens">
+            <div class="item-contents">
+                <div class="col-left">
+                    <h3>Brauciens</h3>
+                </div>
+
+                <div class="col-right">
+                    <div class="text-container">
+                        <h6>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                        </h6>
+                    </div>
+
+                    <div class="button-container">
+                        <a href="{{ url('/brauciens') }}" class="button-white">Lasīt vairāk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-item item-gaidisana">
+            <div class="item-contents">
+                <div class="col-left">
+                    <h3>Gaidīšana</h3>
+                </div>
+
+                <div class="col-right">
+                    <div class="text-container">
+                        <h6>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                        </h6>
+                    </div>
+
+                    <div class="button-container">
+                        <a href="{{ url('/gaidisana') }}" class="button-white">Lasīt vairāk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-item item-pasteiga">
+            <div class="item-contents">
+                <div class="col-left">
+                    <h3>Pasteiga</h3>
+                </div>
+
+                <div class="col-right">
+                    <div class="text-container">
+                        <h6>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+                        </h6>
+                    </div>
+
+                    <div class="button-container">
+                        <a href="{{ url('/pasteiga') }}" class="button-white">Lasīt vairāk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

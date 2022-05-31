c3cb4534305c64fc4a3285985f31e18a99ae3047
niklavsk <niklavsk36@gmail.com>
2022-05-31 12:49:39 +0300

    Attempt to resolve white bar issue on iPhones

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,7 +4,7 @@
         <title>Mans ceļš uz universitāti</title>
         <link rel="icon" href="{{ url('/images/favicon.ico') }}">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script>
 
